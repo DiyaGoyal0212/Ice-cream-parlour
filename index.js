e9e0f1b81b1fa2eca9326d779cf7c83a570a34d4
DIYA GOYAL <goyal.diya0212@gmail.com>
2024-01-01 11:01:54 +0530

    First Deployment

---
+++ index.js
@@ -0,0 +1,16 @@
+var button = document.getElementById('one');
+
+var myfunc = function( ){
+    document.body.style.backgroundImage = "url('ye.jpeg')";
+    document.querySelector("ul").style.color = "red";
+   
+
+}
+var myfunc2 = function( ){
+    document.body.style.backgroundImage = "url('aa.jpg')";
+    document.querySelector("ul").style.color = "white";
+   
+
+}
+
+
